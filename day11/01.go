eb234da886562440efd73a2c9cc921289158e5e9
nitikhon <140893068+nitikhon@users.noreply.github.com>
2025-12-21 01:33:17 +0700

    submit day 11 part 1 solution

---
+++ day11/01.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var nodes map[string][]string = make(map[string][]string)
+var ans int = 0
+
+func dfs1(node string) {
+	if node == "out" {
+		ans++
+		return
+	}
+
+	for _, node := range nodes[node] {
+		dfs(node)
+	}
+}
+
+func main() {
+	inputFile := flag.String("input", "example.txt", "Name of the input file")
+	flag.Parse()
+
+	fmt.Printf("Reading input from: %s\n", *inputFile)
+
+	input, err := os.Open(*inputFile)
+	if err != nil {
+		log.Printf("Error opening input file: %v\n", err)
+		return
+	}
+	defer input.Close()
+
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := strings.Split(strings.TrimSpace(scanner.Text()), ":")
+
+		node := line[0]
+		neighbor := strings.Fields(line[1])
+
+		nodes[node] = append(nodes[node], neighbor...)
+	}
+
+	dfs("you")
+
+	fmt.Printf("The Password is %d\n", ans)
+}
